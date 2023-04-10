96105da5b0fd409447f1ced795cc69addfb60298
Aman Kumar <kumaramansingh137@gmail.com>
2023-04-10 17:08:38 +0530

    Add files via upload

---
+++ React-Redux/react-redux-demo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import CakeContainer from './components/CakeContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CakeContainer />
+    </div>
+  );
+}
+
+export default App;
