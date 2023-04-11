376a12a2f68c81553a75d7754d9da3ddc830d6b2
Aman Kumar <kumaramansingh137@gmail.com>
2023-04-11 16:44:30 +0530

    Add files via upload

--- React-Redux/react-redux-demo/src/App.js
+++ React-Redux/react-redux-demo/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import CakeContainer from './components/CakeContainer';
+import { Provider } from 'react-redux'
+import store from './redux/store';
+
 import './App.css';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
-  return (
-    <div className="App">
-      <CakeContainer />
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <CakeContainer />
+                <HooksCakeContainer />
+            </div>
+        </Provider>
+
+    );
 }
 
 export default App;
---
+++ React-Redux/react-redux-demo/src/redux/store.js
@@ -0,0 +1,7 @@
+
+import { configureStore } from '@reduxjs/toolkit'
+import cakeReducer from './cake/cakeReducer'
+
+const store = configureStore({reducer: cakeReducer})
+
+export default store
