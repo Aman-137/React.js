75203d21dfc7d315d82480e63b0c1467cc4fad3c
Aman Kumar <kumaramansingh137@gmail.com>
2023-05-01 16:32:20 +0530

    Add files via upload

--- React-Practice/src/App.jsx
+++ React-Practice/src/App.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import Practice from './components/Practice'
-import FetchApi from './components/FetchApi'
-import DataFetching from './components/DataFetching'
-import DataPost from './components/DataPost'
-import FetchPost from './components/FetchPost'
+import React from "react";
+import Practice from "./components/Practice";
+import FetchApi from "./components/FetchApi";
+import DataFetching from "./components/DataFetching";
+import DataPost from "./components/DataPost";
+import FetchPost from "./components/FetchPost";
+import DataDisplay from "./components/DataDisplay";
+import CheckDelete from "./components/CheckDelete";
 
 const App = () => {
   return (
     <div>
       <Practice />
-      <FetchApi />
+      {/* <FetchApi /> */}
       {/* <DataFetching />
       <DataPost />
       <FetchPost /> */}
-    </div>
-  )
-}
 
-export default App
+      {/* <DataDisplay /> */}
+      <CheckDelete />
+    </div>
+  );
+};
+
+export default App;
